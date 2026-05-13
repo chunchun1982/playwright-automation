f2bbe080295952a5baf322ca78202f85c82a862d
chunchun choudhary <chunchun.choudhary@jxnoida.com>
2026-05-13 17:19:35 +0530

    Added headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: 'https://opensource-demo.orangehrmlive.com',
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
