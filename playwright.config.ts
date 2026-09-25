b153aaee0a130c7494a62f4a5f402308bf2c3185
chunchun choudhary <chunchun.choudhary@ankahealth.ai>
2026-09-25 18:53:14 +0530

    Run Playwright in headless mode on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: 'https://opensource-demo.orangehrmlive.com',
-    headless: false,
+    headless: !!process.env.CI,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
